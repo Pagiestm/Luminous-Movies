bb2abc3ab195880854191913b9e3414713d9f624
Lucas Bertaud <l.bertaud@hotmail.com>
2024-04-17 14:21:50 +0200

    feat(api): user role

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const Users = mongoose.model('Users', {
     pseudo: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {type: String, required: true}
 });
 
 module.exports = Users;
