69f3784b55572922bcf35b637456e8f510952fd9
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-24 16:53:39 +0200

    feat: loading screen with illustraton svg feat: starting add-movie page

--- app_mobile/lib/pages/admin/Films.dart
+++ app_mobile/lib/pages/admin/Films.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Films extends StatefulWidget {
   const Films({super.key});
@@ -12,6 +14,38 @@ class Films extends StatefulWidget {
 class FilmsState extends State<Films> {
   @override
   Widget build(BuildContext context) {
-    return Text("film");
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBar(
+            title: Text('Ajouter un film', style: GoogleFonts.getFont('Sora')),
+            backgroundColor: Colors.black,
+          ),
+          Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 12)),
+          TextField(
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              hintText: "Nom du film",
+              suffixIcon: Material(
+                color: Colors.transparent,
+                child: SvgPicture.asset(
+                  "assets/icons/movie.svg",
+                  width: 24,
+                  height: 24,
+                  color: Color.fromARGB(255, 203, 202, 202),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide(color: Color.fromARGB(255, 85, 84, 84)),
+              ),
+              filled: true,
+              fillColor: Colors.grey.shade700,
+            ),
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
 }
--- app_mobile/lib/splash_screen.dart
+++ app_mobile/lib/splash_screen.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
-import 'navbar.dart'; 
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'navbar.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -22,29 +26,66 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+              top: -125,
+              left: -60,
+              child: Transform.rotate(
+                angle: 0.6,
+                child: Material(
+                  color: Colors.transparent,
+                  child: SvgPicture.asset(
+                    "assets/icons/rouleau-movie-big.svg",
+                    width: 512,
+                    height: 512,
+                    color: Colors.red.shade900,
+                  ),
+                ),
+              )),
+          Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(
-                  Icons.movie,
-                  color: Colors.red.shade800,
-                  size: 30,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Material(
+                      color: Colors.transparent,
+                      child: SvgPicture.asset(
+                        "assets/icons/movie.svg",
+                        width: 48,
+                        height: 48,
+                        color: Colors.red.shade900,
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Text('Luminous Movies',
+                        style: GoogleFonts.sora(
+                          fontSize: 32,
+                        )),
+                  ],
                 ),
-                SizedBox(width: 10),
-                Text(
-                  'Luminous Movies',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
+                SizedBox(height: 30),
+                CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
               ],
             ),
-            SizedBox(height: 30),
-            CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: -60,
+            right: -5,
+            child: Material(
+              color: Colors.transparent,
+              child: SvgPicture.asset(
+                "assets/icons/rouleau-movie.svg",
+                width: 156,
+                height: 156,
+                color: Colors.red.shade900,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
