7a754195b04bc87383ae699281a5723015086f6b
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-04-24 12:27:05 +0200

    style(Connexion, Inscription): add padding for all item

--- app_mobile/lib/pages/Connexion.dart
+++ app_mobile/lib/pages/Connexion.dart
@@ -21,111 +21,128 @@ class ConnexionState extends State<Connexion> {
 
   @override
   Widget build(BuildContext context) {
-    return toRegister ? Inscription() : Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(
-                  Icons.movie,
-                  color: Colors.red.shade800,
-                  size: 30,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Luminous Movies',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-              ],
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 50),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade700,
-              border: Border.all(color: Colors.grey.shade400),
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: TextFormField(
-              controller: emailController,
-              cursorColor: Colors.white,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Email",
-                hintStyle: TextStyle(color: Colors.grey.shade400),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 20)
-              ),
-              validator: (value){
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade700,
-              border: Border.all(color: Colors.grey.shade400),
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: TextFormField(
-              controller: passwordController,
-              obscureText: passenable,
-              cursorColor: Colors.white,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: "Mot de passe",
-                hintStyle: TextStyle(color: Colors.grey.shade400),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 20),
-                suffix: IconButton(onPressed: (){
-                  setState(() {
-                      if(passenable){
-                        passenable = false;
-                      }else{
-                        passenable = true;
-                      }
-                  });
-                }, icon: Icon(passenable == true?Icons.visibility:Icons.visibility_off), color: Colors.grey.shade400)
-              ),
-              validator: (value){
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-          ),
-          SizedBox(height: 20),
-          isLoading ? Center(child:CircularProgressIndicator()) 
-          : ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.red.shade900)
-            ),
-            onPressed: (){
-              if (_formKey.currentState!.validate()) {
-                  setState(() => isLoading = true );
-                  UserAuth()
-                    .fetchUser(emailController.text, passwordController.text)
-                    .catchError((e) => throw e)
-                    .whenComplete(() => setState(() => isLoading = false ));
-              }
-            }, 
-            child: Text('Connexion', style: TextStyle(color: Colors.white))
-          ),
-          TextButton(
-            onPressed: (){
-              setState(() => toRegister = true);
-            }, 
-            child: Text("S'inscrire", style: TextStyle(color: Colors.white),)
-          ),
-        ]
-      ),
-    );
+    return toRegister
+        ? Inscription()
+        : Form(
+            key: _formKey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.movie,
+                        color: Colors.red.shade800,
+                        size: 30,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Luminous Movies',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      margin: EdgeInsets.only(top: 50),
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade700,
+                          border: Border.all(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextFormField(
+                        controller: emailController,
+                        cursorColor: Colors.white,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.grey.shade400),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.only(left: 20)),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade700,
+                          border: Border.all(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextFormField(
+                        controller: passwordController,
+                        obscureText: passenable,
+                        cursorColor: Colors.white,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            hintText: "Mot de passe",
+                            hintStyle: TextStyle(color: Colors.grey.shade400),
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.only(left: 20),
+                            suffix: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (passenable) {
+                                      passenable = false;
+                                    } else {
+                                      passenable = true;
+                                    }
+                                  });
+                                },
+                                icon: Icon(passenable == true
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                color: Colors.grey.shade400)),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.red.shade900)),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() => isLoading = true);
+                              UserAuth()
+                                  .fetchUser(emailController.text,
+                                      passwordController.text)
+                                  .catchError((e) => throw e)
+                                  .whenComplete(
+                                      () => setState(() => isLoading = false));
+                            }
+                          },
+                          child: Text('Connexion',
+                              style: TextStyle(color: Colors.white))),
+                  TextButton(
+                      onPressed: () {
+                        setState(() => toRegister = true);
+                      },
+                      child: Text(
+                        "S'inscrire",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ]),
+          );
   }
 }
