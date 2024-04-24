1460804a9d4011e5f96a7152c7fabd7a7238b5bd
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-24 09:32:03 +0200

    feat: profil page

--- app_mobile/lib/pages/Profil.dart
+++ app_mobile/lib/pages/Profil.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:luminous_movies/models/users.dart';
+
+import '../services/users/users_session.dart';
+import 'Connexion.dart';
 
 class Profil extends StatelessWidget {
   const Profil({super.key});
 
+  User? user = UserSession.getUser();
+
   @override
   Widget build(BuildContext context) {
+    if (user == null) {
+      return Connexion();
+    }
+
     return Center(
       child: Text(
         'Test page 5',
