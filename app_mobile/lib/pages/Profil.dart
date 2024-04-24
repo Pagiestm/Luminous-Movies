94645c9b9d36cf9e2100ae701fc77a1fc336bc88
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-24 11:13:54 +0200

    feat: new icons for navbar feat: adding all icons from figma project

--- app_mobile/lib/pages/Profil.dart
+++ app_mobile/lib/pages/Profil.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:luminous_movies/models/users.dart';
 
 import '../services/users/users_session.dart';
 import 'Connexion.dart';
 
-class Profil extends StatelessWidget {
+class Profil extends StatefulWidget {
   const Profil({super.key});
 
+  @override
+  MyProfil createState() => MyProfil();
+}
+
+class MyProfil extends State<Profil> {
   User? user = UserSession.getUser();
 
   @override
@@ -15,11 +21,34 @@ class Profil extends StatelessWidget {
       return Connexion();
     }
 
-    return Center(
-      child: Text(
-        'Test page 5',
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mon profil',
+          style:
+              TextStyle(fontFamily: 'Sora', fontSize: 24, color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+        centerTitle: false,
       ),
+      body: Column(children: [
+        Center(
+          child: Material(
+              color: Colors.transparent,
+              child: SvgPicture.asset(
+                "assets/icons/user-circle.svg",
+                width: 156,
+                height: 156,
+                color: Colors.red.shade900,
+              )),
+        ),
+        Center(
+            child: Text(
+          'Votre username',
+          style:
+              TextStyle(fontFamily: 'Sora', fontSize: 24, color: Colors.white),
+        )),
+      ]),
     );
   }
 }
