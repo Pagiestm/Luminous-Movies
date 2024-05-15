6c146ab03ebf234782b2e17dcce699debd2dfe3a
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-05-15 19:29:07 +0200

    feat: page découvrir

--- app_mobile/lib/pages/Decouvrir.dart
+++ app_mobile/lib/pages/Decouvrir.dart
@@ -2,8 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:luminous_movies/components/favorites_movies.dart';
+import 'package:luminous_movies/models/categories.dart';
 import 'package:luminous_movies/models/movies.dart';
 import 'package:luminous_movies/models/users.dart';
+import 'package:luminous_movies/services/categories/categories.dart';
 import 'package:luminous_movies/services/movies/movies.dart';
 import 'package:luminous_movies/services/navigation.dart';
 import 'package:luminous_movies/services/users/users_session.dart';
@@ -17,6 +19,7 @@ class Decouvrir extends StatefulWidget {
 
 class _DecouvrirState extends State<Decouvrir> {
   List<Movie> movies = [];
+  List<Categories> categories = [];
   User? user = UserSession.getUser();
   FavoritesMovies? widgetFavoritesMovies;
 
@@ -24,6 +27,7 @@ class _DecouvrirState extends State<Decouvrir> {
   void initState() {
     super.initState();
     fetchMovies();
+    fetchCategories();
   }
 
   void fetchMovies() async {
@@ -34,6 +38,14 @@ class _DecouvrirState extends State<Decouvrir> {
     });
 
     print(movies[0].categories);
+  }
+
+  void fetchCategories() async {
+    CategoriesService categoriesService = CategoriesService();
+    var fetchedCategories = await categoriesService.fetchCategories();
+    setState(() {
+      categories = fetchedCategories.reversed.toList();
+    });
   }
 
   @override
@@ -49,65 +61,82 @@ class _DecouvrirState extends State<Decouvrir> {
         backgroundColor: Colors.black,
         centerTitle: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
-        child: Container(
-          height: 400, // Définit la hauteur du slider d'images
-          child: PageView.builder(
-            controller: PageController(
-                viewportFraction:
-                    0.8), // Affiche une petite partie des images suivantes
-            itemCount: movies.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () async {
-                  final bool? shouldRefresh = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FutureBuilder(
-                            future: Navigation.getInstance()
-                                .toMovieDetailsPage(user, movies[index]),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return Center(
-                                  child: SizedBox(
-                                    height: 40,
-                                    width: 40,
-                                    child: CircularProgressIndicator(),
-                                  ),
-                                );
-                              } else if (snapshot.hasError) {
-                                return Text('Erreur: ${snapshot.error}');
-                              } else {
-                                return snapshot.data!;
-                              }
-                            })),
-                  );
-                  if (shouldRefresh == true && user != null) {
-                    setState(() {
-                      widgetFavoritesMovies = FavoritesMovies(
-                        key: UniqueKey(),
-                      );
-                    });
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  // Ajoute des marges à gauche et à droite de chaque image
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                        10), // Arrondit les bords de l'image
-                    child: CachedNetworkImage(
-                      imageUrl: movies[index].image,
-                      fit: BoxFit.cover,
+      body: Column(
+        children: [
+          for (var category in categories)
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    category.name,
+                    style: GoogleFonts.sora(
+                      fontSize: 24,
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        ),
+                Container(
+                  height: 400, // Définit la hauteur du slider d'images
+                  child: PageView.builder(
+                    controller: PageController(viewportFraction: 0.8),
+                    // Affiche une petite partie des images suivantes
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () async {
+                          final bool? shouldRefresh = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FutureBuilder(
+                                    future: Navigation.getInstance()
+                                        .toMovieDetailsPage(
+                                            user, movies[index]),
+                                    builder: (context, snapshot) {
+                                      if (snapshot.connectionState ==
+                                          ConnectionState.waiting) {
+                                        return Center(
+                                          child: SizedBox(
+                                            height: 40,
+                                            width: 40,
+                                            child: CircularProgressIndicator(),
+                                          ),
+                                        );
+                                      } else if (snapshot.hasError) {
+                                        return Text(
+                                            'Erreur: ${snapshot.error}');
+                                      } else {
+                                        return snapshot.data!;
+                                      }
+                                    })),
+                          );
+                          if (shouldRefresh == true && user != null) {
+                            setState(() {
+                              widgetFavoritesMovies = FavoritesMovies(
+                                key: UniqueKey(),
+                              );
+                            });
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          // Ajoute des marges à gauche et à droite de chaque image
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                10), // Arrondit les bords de l'image
+                            child: CachedNetworkImage(
+                              imageUrl: movies[index].image,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            )
+        ],
       ),
     );
   }
