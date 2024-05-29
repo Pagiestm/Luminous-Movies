35cdfb8bfeb6b65530f3b35e0ff4e54844f2a64a
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-05-29 22:30:17 +0200

    feat(Decouvrir): add circularIndicator

--- app_mobile/lib/pages/Decouvrir.dart
+++ app_mobile/lib/pages/Decouvrir.dart
@@ -23,6 +23,8 @@ class _DecouvrirState extends State<Decouvrir> {
   User? user = UserSession.getUser();
   FavoritesMovies? widgetFavoritesMovies;
   int displayedCategoriesCount = 5;
+  bool isLoadingMovies = true;
+  bool isLoadingCategories = true;
 
   ScrollController _scrollController = ScrollController();
 
@@ -68,6 +70,7 @@ class _DecouvrirState extends State<Decouvrir> {
     var fetchedMovies = await movieService.fetchMovies();
     setState(() {
       movies = fetchedMovies.reversed.toList();
+      isLoadingMovies = false;
     });
   }
 
@@ -76,6 +79,7 @@ class _DecouvrirState extends State<Decouvrir> {
     var fetchedCategories = await categoriesService.fetchCategories();
     setState(() {
       categories = fetchedCategories.reversed.toList();
+      isLoadingCategories = false;
     });
   }
 
@@ -92,114 +96,116 @@ class _DecouvrirState extends State<Decouvrir> {
         backgroundColor: Colors.black,
         centerTitle: false,
       ),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 80.0),
-          child: Column(
-            children: [
-              ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: displayedCategoriesCount,
-                itemBuilder: (context, index) {
-                  if (index >= categories.length) {
-                    return SizedBox.shrink();
-                  }
-                  var moviesByCategory = [];
+      body: isLoadingMovies || isLoadingCategories
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              controller: _scrollController,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 80.0),
+                child: Column(
+                  children: [
+                    ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: displayedCategoriesCount,
+                      itemBuilder: (context, index) {
+                        if (index >= categories.length) {
+                          return SizedBox.shrink();
+                        }
+                        var moviesByCategory = [];
 
-                  for (var movie in movies) {
-                    for (var cat in movie.categories) {
-                      if (cat == categories[index].name) {
-                        moviesByCategory.add(movie);
-                      }
-                    }
-                  }
+                        for (var movie in movies) {
+                          for (var cat in movie.categories) {
+                            if (cat == categories[index].name) {
+                              moviesByCategory.add(movie);
+                            }
+                          }
+                        }
 
-                  if (moviesByCategory.isEmpty) {
-                    return SizedBox.shrink();
-                  }
+                        if (moviesByCategory.isEmpty) {
+                          return SizedBox.shrink();
+                        }
 
-                  return Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "${categories[index].name.substring(0, 1).toUpperCase()}${categories[index].name.substring(1).toLowerCase()}",
-                          style: GoogleFonts.sora(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          controller: PageController(viewportFraction: 0.4),
-                          itemCount: moviesByCategory.length,
-                          itemBuilder: (context, index) {
-                            return GestureDetector(
-                              onTap: () async {
-                                final bool? shouldRefresh =
-                                    await Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => FutureBuilder(
-                                          future: Navigation.getInstance()
-                                              .toMovieDetailsPage(user,
-                                                  moviesByCategory[index]),
-                                          builder: (context, snapshot) {
-                                            if (snapshot.connectionState ==
-                                                ConnectionState.waiting) {
-                                              return Center(
-                                                child: SizedBox(
-                                                  height: 40,
-                                                  width: 40,
-                                                  child:
-                                                      CircularProgressIndicator(),
-                                                ),
-                                              );
-                                            } else if (snapshot.hasError) {
-                                              return Text(
-                                                  'Erreur: ${snapshot.error}');
-                                            } else {
-                                              return snapshot.data!;
-                                            }
-                                          })),
-                                );
-                                if (shouldRefresh == true && user != null) {
-                                  setState(() {
-                                    widgetFavoritesMovies = FavoritesMovies(
-                                      key: UniqueKey(),
-                                    );
-                                  });
-                                }
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 12.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: CachedNetworkImage(
-                                    imageUrl: moviesByCategory[index].image,
-                                    fit: BoxFit.cover,
-                                  ),
+                        return Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "${categories[index].name.substring(0, 1).toUpperCase()}${categories[index].name.substring(1).toLowerCase()}",
+                                style: GoogleFonts.sora(
+                                  fontSize: 20,
                                 ),
                               ),
-                            );
-                          },
-                        ),
-                      ),
-                      Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 24)),
-                    ],
-                  );
-                },
+                            ),
+                            Container(
+                              height: 200,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                controller: PageController(viewportFraction: 0.4),
+                                itemCount: moviesByCategory.length,
+                                itemBuilder: (context, index) {
+                                  return GestureDetector(
+                                    onTap: () async {
+                                      final bool? shouldRefresh =
+                                          await Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => FutureBuilder(
+                                                future: Navigation.getInstance()
+                                                    .toMovieDetailsPage(user,
+                                                        moviesByCategory[index]),
+                                                builder: (context, snapshot) {
+                                                  if (snapshot.connectionState ==
+                                                      ConnectionState.waiting) {
+                                                    return Center(
+                                                      child: SizedBox(
+                                                        height: 40,
+                                                        width: 40,
+                                                        child:
+                                                            CircularProgressIndicator(),
+                                                      ),
+                                                    );
+                                                  } else if (snapshot.hasError) {
+                                                    return Text(
+                                                        'Erreur: ${snapshot.error}');
+                                                  } else {
+                                                    return snapshot.data!;
+                                                  }
+                                                })),
+                                      );
+                                      if (shouldRefresh == true && user != null) {
+                                        setState(() {
+                                          widgetFavoritesMovies = FavoritesMovies(
+                                            key: UniqueKey(),
+                                          );
+                                        });
+                                      }
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 12.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: CachedNetworkImage(
+                                          imageUrl: moviesByCategory[index].image,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 24)),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 
